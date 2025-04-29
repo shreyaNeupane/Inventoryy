0bcec9b71fdb357783a7575abcbc60afe1499eac
shreyaNeupane <shreyaneupane49@gmail.com>
2025-04-29 13:49:05 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "../src/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>hello</Button>
+    </>
+  );
+}
+
+export default App;
